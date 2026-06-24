78cb2e3d66cd02103bac296cfdad5272ce212618
debugwith-Vaishnavi <vaishnavishinde8795@gmail.com>
2026-06-24 08:43:36 +0530

    Fix Cloudflare deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/vaishnavi-shinde-portfolio/",
   plugins: [
     react(),
     tailwindcss(),
